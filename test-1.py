164f7979a77fe4248c812ef42a6ee1f978422b35
Navya <navyaburrewar2721@gmail.com>
2026-06-06 12:35:51 +0530

    peak

---
+++ test-1.py
@@ -0,0 +1,20 @@
+
+# Element greater than neighbors.
+
+# Example:
+
+# [1,2,5,7,6,3]
+# peak =7
+
+def peak(arr): 
+    low=0
+    high=len(arr)-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]<arr[mid+1]:
+            low=mid+1
+        else:
+            high=mid 
+    return low
+
+print(peak([1,2,5,7,6,3]))
