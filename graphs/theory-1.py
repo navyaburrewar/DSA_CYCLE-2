b22302e1dc0a42fb08c24613ac232883d98ae9fc
Navya <navyaburrewar2721@gmail.com>
2026-08-24 22:43:54 +0530

    test-1

---
+++ graphs/theory-1.py
@@ -0,0 +1,53 @@
+# # graphs :
+# #  relationship or connection b/w the objects
+# #  graphs contains: nodes/vertices and connection/edges
+
+# #  graphs -->edges ,vertices
+# # two types of graphs
+# # directed and undirected
+
+
+# #  difference between the cycles and tress
+
+# # tress does not have cycle
+# # graphs will have the cycles
+# #  a graph cannot  be a tree and 
+# # tree can be  graphs 
+
+# # using grphs we can revisit the nodes
+
+
+
+# # types of graphs
+# # directed graph
+# # undirectted graph
+# # weighted graph
+# # unweighted graph
+# # adjacency list
+
+# # graphs programming:
+
+# graph={}
+
+# n=int(input("enter no.of nodes"))
+
+# for i in range(n):
+#     node=input("enter nodes: ")
+#     graph [node]=[]
+# e=int(input("enter no.of edges: "))
+
+# for j in range(e):
+#     print("enter edges: ",i+1)
+#     n1=input("enter 1st node: ")
+#     n2=input("enter 2 nd node: ")
+
+#     graph[n1].append(v2)
+#     graph[n2].append(v1)
+
+# print("adjaecncy lst: ")
+
+
+
+
+
+
