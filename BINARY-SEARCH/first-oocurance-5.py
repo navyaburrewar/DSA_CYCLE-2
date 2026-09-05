233dd81bc66a8004d261eede6b6c52be7715b4f0
Navya <navyaburrewar2721@gmail.com>
2026-09-05 23:33:30 +0530

    choti123

--- BINARY-SEARCH/first-oocurance-5.py
+++ BINARY-SEARCH/first-oocurance-5.py
@@ -18,5 +18,5 @@ def first_occurance(arr,target):
             end=mid-1
     return ans
 
-print(first_occurance([1,11,2,0,3,1,5,5],3))
+print(first_occurance([18,11,2,0,3,1,5,5],3))
                
