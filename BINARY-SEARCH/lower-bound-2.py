f291c9bd3b8ae476eb080f19241838d0ddfb728f
Navya <navyaburrewar2721@gmail.com>
2026-09-22 23:59:59 +0530

    0987

--- BINARY-SEARCH/lower-bound-2.py
+++ BINARY-SEARCH/lower-bound-2.py
@@ -27,4 +27,4 @@ def lower_bound(arr, target):    # [1,2,3,4,6]    t=4
             low = mid + 1
 
     return ans
-print(lower_bound([100,200,60,81,561],89))
+print(lower_bound([100,200,60,81,561],22))
